54cf871de55430bee659a1402ba4cc77083e3042
philmst <75762221+philmst@users.noreply.github.com>
2021-09-25 14:16:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div class="box-img">
+        <img src="https://assets.codepen.io/6799589/train-station_1.jpg" alt="train station" />
+      </div>
+
+      <div class="box-text">
+        <p>Train Station</p>
+      </div>
+
+      <div class="box-img">
+        <img src="https://assets.codepen.io/6799589/coffee_1.jpg" alt="morning coffee" />
+      </div>
+
+      <div class="box-text">
+        <p> Coffee </p>
+      </div>
+
+      <div class = 'box-img'>
+        <img src="https://assets.codepen.io/6799589/gull_1.jpg" alt="sea gull"/>
+      </div>
+
+      <div class="box-text">
+        <p> Sea Gull </p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
